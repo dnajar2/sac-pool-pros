66ded99d258b9308385945dab68e6d1eddef6dc8
David Najar <dnajar2@gmail.com>
2018-02-15 15:50:46 -0800

    remove con from public repo

--- www/admin/conn.php
+++
@@ -1,9 +0,0 @@
-<?php
-$hostname = "localhost";
-$database = "sacpoolp_spp";
-$username = "sacpoolp_spp";
-$password = "rGdb@@m#.bEu";
-
-$conn = mysqli_connect($hostname, $username, $password, $database); 
-
-?>
